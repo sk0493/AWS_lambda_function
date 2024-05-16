a315d09d251b024441839082813e677d536f7621
sk0493 <166520562+sk0493@users.noreply.github.com>
2024-05-17 00:31:10 +0100

    Added comments to lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
 import json
-import degrees_of_kb
+import degrees_of_kb # imported from github link from requirements.txt
 
 def lambda_handler(event, context):
-    
+    # main function for AWS lambda function
     k_b = degrees_of_kb.KevinBacon6Degrees("/wiki/Six_Degrees_of_Kevin_Bacon")
     k_b.generate_6_degrees()
     
@@ -10,3 +10,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(k_b.get_urls())
     }
+
+# Entry point for local execution
+if __name__ == "__main__":
+    lambda_handler({}, {})
